eacc01a574bc174d651b3d9c2ce7788f175a1207
franbanbee <129038669+franbanbee@users.noreply.github.com>
2023-08-26 17:39:17 +0800

    .js css OK

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from '../styles/about.module.css';
+
+const about = () => {
+    return (
+        <div>about
+
+
+            <button className={styles.button_blue}>button</button>
+        </div>
+    )
+}
+export default about
---
+++ pages/news.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from '../styles/Layout.module.css';
+
+const news = () => {
+    return (
+        <div>news
+
+
+            <button className={styles.button_blue}>button</button>
+        </div>
+    )
+}
+export default news
